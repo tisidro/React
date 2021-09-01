c558aa712349f75ae71d0ae64dc97fa23043e1d3
Terri Isidro <terri.isidro@gmail.com>
2021-09-01 16:19:01 -0400

    learning to use React, here are some files where I set up a few page components and use dynamic value in my template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const title = 'This is a dynamic title!'
+  const likes = 50;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content"></div>
+      <h1>{title}</h1>
+      <p>Number of likes: {likes}</p>
     </div>
   );
 }
