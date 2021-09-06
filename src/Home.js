0ec3ba31c5afe6c6864cde55acbdb71eba95b3ac
Terri Isidro <terri.isidro@gmail.com>
2021-09-06 13:13:01 -0400

    added code using useState to create dynamic variables in my page using the click event!

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,27 @@
+import { useState } from 'react';
+
 const Home = () => {
 
+    const [name, setName] = useState('Terri');//sets in. value
+    const [age, setAge] = useState(20);
     const handleClick = () => {
-        console.log('Hi there!');
+        setName('Tina');
+        setAge(4);
     }
     const handleClickTwo = (name) => {
         console.log('Hi' + name);
 
     }
+
+
     return (
 
         <div className="home">
             <h2>Homepage</h2>
+            <p> {name} is {age} years old</p>
             <button onClick={handleClick}> Click Me</button>
-            <button onClick={() => {
-                handleClickTwo('Terri')
-            }}> Click Again</button>
+            <button onClick={() => handleClickTwo('Terri')
+            }> Click Again</button>
         </div>
     );
 }
