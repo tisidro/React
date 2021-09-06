864ddcab9b0d537bb231e3cb760a620dd0ce5d8a
Terri Isidro <terri.isidro@gmail.com>
2021-09-06 12:05:13 -0400

    adding a button with function that accepts an argument. you need to wrap the click event argument in an anonymous function for it to take your input!

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Hi there!');
+    }
+    const handleClickTwo = (name) => {
+        console.log('Hi' + name);
+
+    }
     return (
 
         <div className="home">
             <h2>Homepage</h2>
-
+            <button onClick={handleClick}> Click Me</button>
+            <button onClick={() => {
+                handleClickTwo('Terri')
+            }}> Click Again</button>
         </div>
     );
 }
